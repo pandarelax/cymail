989760fe573d27041c9ca9ebd4884f67d8aebff2
Efehan Turhan <94855256+pandarelax@users.noreply.github.com>
2023-12-27 23:55:40 +0300

    Add morgan and helmet

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+// Import required libraries
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieparser from 'cookie-parser'
+import cors from 'cors';
+import compression from 'compression';
+import logger from 'morgan';
+import helmet from 'helmet';
+import path from 'path';
+
+// Create an instance of Express
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(logger('dev'));
+app.use(compression());
+app.use(cookieparser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+const port: string | 3000 = process.env.PORT || 3000;
